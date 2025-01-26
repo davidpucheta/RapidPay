5b03a6f60c57939c8a2118e8e721fd8062a672a0
David Reyes Pucheta <davidpucheta@gmail.com>
2025-01-26 12:32:29 -0600

    Update card validator

--- RapidPay/Validators/CardValidator.cs
+++ RapidPay/Validators/CardValidator.cs
@@ -1,17 +1,20 @@
 ﻿using FluentValidation;
+using RapidPay.Data;
 using RapidPay.Model.Api.Request;
 
 namespace RapidPay.Validators
 {
     public class CardValidator : AbstractValidator<CreateCardRequest>
     {
-        public CardValidator()
+        public CardValidator(ApplicationDbContext dbContext)
         {
             RuleFor(c => c.Balance).GreaterThanOrEqualTo(0);
             RuleFor(c => c.CardNumber)
                 .NotEmpty()
                 .Length(15)
-                .Must(IsValidCardNumber).WithMessage("'{PropertyName}' should be all numbers");
+                            .Must(IsValidCardNumber).WithMessage("'{PropertyName}' should be all numbers")
+            .Must(n => dbContext.Card.FirstOrDefault(c => c.CardNumber == n) == null)
+                .WithMessage("'{PropertyName}' already registered.");
         }
 
         private bool IsValidCardNumber(string cardNumber)
