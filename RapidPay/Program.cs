ef1ecf902b452daddb8a4739103f9c8f7d199fe4
David Reyes Pucheta <davidpucheta@gmail.com>
2025-01-26 12:28:40 -0600

    Add card validator

--- RapidPay/Program.cs
+++ RapidPay/Program.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using RapidPay.Data;
 
@@ -11,6 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ RapidPay/Validators/CardValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using RapidPay.Model.Api.Request;
+
+namespace RapidPay.Validators
+{
+    public class CardValidator : AbstractValidator<CreateCardRequest>
+    {
+        public CardValidator()
+        {
+            RuleFor(c => c.Balance).GreaterThanOrEqualTo(0);
+            RuleFor(c => c.CardNumber)
+                .NotEmpty()
+                .Length(15)
+                .Must(IsValidCardNumber).WithMessage("'{PropertyName}' should be all numbers");
+        }
+
+        private bool IsValidCardNumber(string cardNumber)
+        {
+            return cardNumber.All(char.IsNumber);
+        }
+    }
+}
